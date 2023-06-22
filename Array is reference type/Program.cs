8a48c7a14a4a56cfeb679c04dbdeec1fbdbfc051
mkhoyanarmen <93216230+mkhoyanarmen@users.noreply.github.com>
2023-06-22 14:33:23 +0400

    Add project files.

---
+++ Array is reference type/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Array_is_reference_type
+{
+    internal class Program
+    {
+        public static void WorkWithArray(int[] arr)
+        {
+            arr[0] = 123;
+        }
+        static void Main(string[] args)
+        {
+            int[] na5 = { 1, 2, 3, 4, 5 };
+            foreach (var v in na5) // 1 2 3 4 5
+                Console.Write(v + " ");
+
+            Console.WriteLine();
+            WorkWithArray(na5);
+            foreach (var v in na5) // 123 2 3 4 5
+                Console.Write(v + " ");
+            Console.WriteLine();
+        }
+    }
+}
